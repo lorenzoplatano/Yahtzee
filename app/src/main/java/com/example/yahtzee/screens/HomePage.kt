716d5f65290ee625affb65cac7a9253fbd7078cf
andrea <andrea.gargiulo2006@gmail.com>
2025-05-15 17:09:49 +0200

    Try message

--- app/src/main/java/com/example/yahtzee/screens/HomePage.kt
+++ app/src/main/java/com/example/yahtzee/screens/HomePage.kt
@@ -22,6 +22,7 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -38,12 +39,24 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Homepage (navController: NavController) {
+    val context = LocalContext.current
 
     var showExitDialog by remember { mutableStateOf(false) }
 
     val colors = MaterialTheme.colorScheme
+
+
+
+    BackHandler(enabled = true) {
+        showExitDialog = true
+    }
+    if (showExitDialog) {
+        AlertDialog(
+            onDismissRequest = { showExitDialog = false },
+            title = { Text("Exit App)},
 }
 
 
